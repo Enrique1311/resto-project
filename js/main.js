fba7ba7a1d788583e2b90e93ba89c4b6d7df490b
Enrique1311 <ejspinelli131176@gmail.com>
2023-03-25 14:51:20 -0300

    The navbar and the home's image wrer added

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const hamburguerMenu = document.querySelector(".hamburguer-menu");
+const mobileLinks = document.querySelector(".navbar-links");
+const closeMenuIcon = document.querySelector(".close-menu-icon");
+
+hamburguerMenu.addEventListener("click", toggleMenu);
+closeMenuIcon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+	mobileLinks.classList.toggle("inactive");
+}
