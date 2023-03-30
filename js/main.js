1838e50ce8c465e77cf3283884fbabfab2ed51e4
Enrique1311 <ejspinelli131176@gmail.com>
2023-03-30 01:07:20 -0300

    The footer was added

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 const hamburguerMenu = document.querySelector(".hamburguer-menu");
-const mobileLinks = document.querySelector(".navbar-links");
+const mobileLinks = document.querySelector(".mobile-navbar-links");
 const closeMenuIcon = document.querySelector(".close-menu-icon");
 
 hamburguerMenu.addEventListener("click", toggleMenu);
 closeMenuIcon.addEventListener("click", toggleMenu);
+mobileLinks.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	mobileLinks.classList.toggle("inactive");
+	mobileLinks.classList.toggle("opened-menu");
 }
